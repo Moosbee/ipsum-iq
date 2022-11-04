c7de736cabec4b7e188435262ba413f06bde8e70
Moruk16 <zabokrecina94@gmail.com>
2022-11-04 16:08:28 +0100

    changes

--- server/database.js
+++ server/database.js
@@ -11,25 +11,18 @@ const pool = createPool({
 })
 
 
-async function database_output(username, passwd) {
+function database_output(username, passwd) {
     
-    console.log(username);
-    console.log(passwd);
 }
-
-database_output().then(pool.query('SELECT username, password FROM user', (err, result, fields)=>{
+pool.query('SELECT username, password FROM user', (err, result, fields)=>{
 
     if(err) {
         return console.log(err);
     }
-    //Ergebnise der Abfrage in Variablen speichern
 
-    var plsgeh = result;
-    console.log(plsgeh);
-
-    return plsgeh;
+    return console.log(result);
    
-}));
+});
 
 
 
