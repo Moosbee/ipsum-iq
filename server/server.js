202d03a91a1c689029e2a7762f261164998c253d
Moruk16 <zabokrecina94@gmail.com>
2022-12-11 14:11:38 +0100

    Session Cookie Teil 1

--- server/server.js
+++ server/server.js
@@ -3,20 +3,40 @@ const app = express();
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const database = require('./database');
+const session = require("express-session");
+const store = new session.MemoryStore();
+
 const mysql = require('mysql2/promise');
 
+const connInfo={
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "ipsum_iq",
+    connectionLimit: 10
+}
+
+app.use(session({
+    secret:'secretkey',
+    cookie: {maxAge: 300000},
+    saveUninitialized: false,
+    store
+}));
+
+const connection = mysql.createConnection(connInfo);
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.get('/login', (req, res) => {
-  res.sendFile('/login.html', { root: __dirname });
+app.get('/', (req, res) => {
+    console.log(store);
+    res.send("test");
 });
 
+//Register Code
 app.post('/users', async (req, res) => {
-
     try {
 
         const salt = await bcrypt.genSalt(10);
@@ -28,7 +48,7 @@ app.post('/users', async (req, res) => {
         const query = `INSERT INTO user (username, password) VALUES ("${user.name}", "${user.password}")`;
         res.status(201).send("Successfully registered");
         
-        database.pool.query(query, (err, result, fields) => {
+        connection.pool.query(query, (err, result, fields) => {
             if (result) {
                 return console.log(result);
             }
@@ -44,27 +64,46 @@ app.post('/users', async (req, res) => {
 
 });
 
+//Login Code
 app.post('/login', async (req, res) => {
 
+    console.log(req.sessionID)
     let an = await (await connection).query("SELECT username, password FROM user");
 
-    let isUser = req.body.name === an[0][0].username;
-    let isPassword = await bcrypt.compare(req.body.password, an[0][0].password);
+    console.log(an);
+    
+    let isAdmin = req.body.name === an[0][0].username;
+    let isAdminPassword = await bcrypt.compare(req.body.password, an[0][0].password);
+    let isUser = req.body.name === an[0][1].username;
+    let isUserPassword = await bcrypt.compare(req.body.password, an[0][1].password);
 
-    if (an == null) {
-        return res.status(400).send("Cannot find user");
-    }
-    try {
-        if (isUser && isPassword) {
-            res.send("Success");
+    if (an) {
+        if (req.session.authenticated) {
+            res.json(req.session);
+            console.log("moruk");
         }
-        
+        else if(isAdmin && isAdminPassword) {
+            req.session.authenticated = true;
+            req.session.user = an[0][0].username;
+            res.json(req.session);
+            console.log("Admin Cookie set");
+        }
+
+        else if(isUser && isUserPassword) {
+            req.session.authenticated = true;
+            req.session.user = an[0][0].username;
+            res.json(req.session);
+            console.log("User Cookie set");
+        }
+            
         else {
-            res.send("Wrong User or Password");
+            res.send("Wrong Username or Password");
         }
-    } catch {
+    }
+    else {
         res.status(500).send("Fatal error :(");
     }
+
 });
 
 app.listen(3000);
