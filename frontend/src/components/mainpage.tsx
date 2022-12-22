a52f76cad430e82ead10a0d22c896827d975f7ac
Moruk16 <zabokrecina94@gmail.com>
2022-12-22 14:26:37 +0100

    Authentifizierung Main Funktionen fertig

--- frontend/src/components/mainpage.tsx
+++ frontend/src/components/mainpage.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import {BrowserRouter as Router, Navigate, Route} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Mainpage = () => {
+
+    const navigate = useNavigate();
+
+    Axios.defaults.withCredentials = true;
+
+    useEffect(()=> {
+        Axios.get('http://localhost:3001/Mainpage').then((Response) => {
+    
+        if(Response.data.LoggedIn) {
+          console.log("LOGGED IN");
+          navigate("/Mainpage");
+    
+        }
+        else {
+          console.log("LOGGED out");
+          navigate("/");
+        }
+            
+        });
+      }, []);
+
+
     return (
         <>
             <h1>mainpage</h1>
