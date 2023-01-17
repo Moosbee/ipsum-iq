522e81a119d54cc1386ec2298bada7245434facb
Moruk16 <zabokrecina94@gmail.com>
2023-01-17 16:18:14 +0100

    Einträge SQL abfrage Limit 15

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.use(cors({
 
 app.get('/entries', async (req, res) => {
 
-    let queryresult = await (await connection).query("SELECT user, Zeitpunkt, Licht, Status, Datum FROM eintraege ORDER BY eintraege_id DESC;");
+    let queryresult = await (await connection).query("SELECT user, Zeitpunkt, Licht, Status, Datum FROM eintraege ORDER BY eintraege_id DESC LIMIT 15;");
 
     if (req.session.user) {
         if (req.session.user == "admin") {
@@ -294,7 +294,7 @@ wss.on('connection', ws => {
 IO.on('connection', (socket) => {
 
     console.log('a user connected');
-
+    
     socket.on("test_message", (data) => {
         console.log("THIS IS THE DATA: " + data.message);
       })
